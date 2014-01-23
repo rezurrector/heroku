24b25a1184df203f2c626d6e17eab88708ce4176
angel marquez <angel.marquez@gmail.com>
2014-01-22 19:09:23 -0800

    changed mongo connection string

--- routes/wines.js
+++ routes/wines.js
@@ -4,7 +4,7 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
+var server = new Server('mongodb://heroku_app21484370:kdo59vg9ho0fid16jji95q18bs@ds027409.mongolab.com:27409/heroku_app21484370', 27017, {auto_reconnect: true});
 db = new Db('winedb', server, {safe: true});
 
 db.open(function(err, db) {
